6c0143c290e52276c307bf7a9482400705a18aa2
ChaimaaAsri <108627246+Chaimaa-as@users.noreply.github.com>
2023-07-08 12:52:22 +0200

    goalItem

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -48,17 +48,15 @@ function Dashboard() {
 
       <section className='content'>
         {goals.length > 0 ? (
-          <div className='goals'>
+          <div className="goals">
             {goals.map((goal) => (
-              <GoalItem key={goal._id} goal={goal} />
+              <GoalItem key={goal._id} goal={goal}/>
             ))}
           </div>
-        ) : (
-          <h3>You have not set any goals</h3>
-        )}
+        ) : (<h3>You have not set any goals</h3>)}
       </section>
     </>
-  );
+  )
 }
 
 export default Dashboard;
